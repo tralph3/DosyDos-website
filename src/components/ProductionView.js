ad1e4973451b9c4cf383b8dd5165d7d8f887780b
tralph3 <tomasralph2000@gmail.com>
2024-05-31 20:46:25 -0300

    add technical sheet in production view

--- src/components/ProductionView.js
+++ src/components/ProductionView.js
@@ -5,6 +5,17 @@ import { getConfigAndProductions } from "../util";
 import ReactPlayer from "react-player";
 import { Footer } from "./Footer";
 
+function SheetEntry({ role, name }) {
+    return (
+        <div className="sheet-entry">
+            {role}
+            <span className="sheet-right-side">
+                {name}
+            </span>
+        </div>
+    );
+}
+
 function ProductionView() {
   let { prodName } = useParams();
   const [productions, setProductions] = useState(Object);
@@ -22,10 +33,21 @@ function ProductionView() {
   if (!productions[prodName]) {
     return <div>Unknown production</div>;
   }
-
   let contactConfig = config["contact"];
 
-  let production = productions[prodName];
+    let production = productions[prodName];
+    let techSheetEntries = [];
+    for (let entryKey in production["sheet"]) {
+        let sheetEntry = production["sheet"][entryKey];
+        techSheetEntries.push(
+            <SheetEntry
+                key={entryKey}
+                role={sheetEntry[0]}
+                name={sheetEntry[1]}
+            />,
+        );
+    }
+
   return (
     <div>
       <div className="video-container">
@@ -47,7 +69,10 @@ function ProductionView() {
       </div>
       <p className="pview-title">{production.title}</p>
       <p className="pview-date">{production.releaseDate}</p>
-      <p className="pview-description">{production.description}</p>
+        <p className="pview-description">{production.description}</p>
+        <div className="pview-tech-sheet">
+            {techSheetEntries}
+            </div>
       <div style={{ "margin-top": "15vh" }} />
       <Footer contactConfig={contactConfig} />
     </div>
